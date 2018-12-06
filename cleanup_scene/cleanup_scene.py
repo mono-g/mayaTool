ba34d575732539257d9bc262b45c1ffd5193bf33
mono-g <monostream.g@gmail.com>
2018-12-06 14:46:58 +0900

    [add]new files

---
+++ cleanup_scene/cleanup_scene.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##########################################################
+#
+#  @brief  clean up scene
+#  @file   cleanup_scene.py
+#  @author mono-g
+#
+#  Copyright(C) 2018 Satoshi Gonokami.
+#
+##########################################################
+import pymel.core as pm
+
+
+# ---------------------------------------------------------
+# clean up PanelCallback
+# ---------------------------------------------------------
+# @param None
+# @return None
+def cleanupPanelCallback():
+    viewName = ['Top View', 'Side View', 'Front View', 'Persp View']
+    for vname in viewName:
+        panelName = pm.sceneUIReplacement(gp=['modelPanel', pm.mel.localizedPanelLabel(vname)])
+        if panelName != '':
+            pm.modelEditor(panelName, e=True, ec='')
